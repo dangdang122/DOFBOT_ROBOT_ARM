bbbd70d210520ad4257471053a3a06ec2d728b09
dangdang122 <vmfhxhtm122@naver.com>
2026-03-09 17:35:51 +0900

    FK 계산 및 J1~ J6 상태 추적 기능 추가
    로봇의 실제 하드웨어 상태를 기반으로 엔드이펙터 좌표를 계산하기 위해 FK 기반 상태 동기화 기능을 구현했다

--- dofbot_simple.py
+++ dofbot_simple.py
@@ -8,6 +8,7 @@ import ikpy.chain
 class DofbotSimple:
 
     def __init__(self, urdf_file_name):
+
         self.Arm = Arm_Device()
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,11 +20,41 @@ class DofbotSimple:
         )
 
         self.gripper_angle = 90
-        self.last_pos = [0.0, 0.0, 0.0]
-        self.last_joints = [0.0] * 7
+        self.last_pos = [0.0,0.0,0.0]
+        self.last_joints = [0.0]*7
 
-    def set_gripper(self, angle, duration_ms=1500):
-        angle = int(max(10, min(170, angle)))
+        self.sync_state_from_hardware()
+
+    def sync_state_from_hardware(self):
+
+        real_joints = [0.0]*7
+        degrees = [0]*6
+
+        for i in range(1,7):
+
+            angle = self.Arm.Arm_serial_servo_read(i)
+
+            if angle is None:
+                angle = 90
+
+            degrees[i-1] = angle
+
+            if i <= 5:
+                real_joints[i] = math.radians(angle - 90)
+
+        self.gripper_angle = degrees[5]
+
+        T = self.chain.forward_kinematics(real_joints)
+        pos = T[:3,3]
+
+        self.last_joints = real_joints
+        self.last_pos = [pos[0],pos[1],pos[2]]
+
+        return degrees
+
+    def set_gripper(self,angle,duration_ms=1500):
+
+        angle = int(max(10,min(170,angle)))
 
         if self.gripper_angle == angle:
             return
@@ -36,17 +67,14 @@ class DofbotSimple:
             int(duration_ms)
         )
 
-    def move_to_xyz(self, x, y, z, duration_ms=1500):
+    def move_to_xyz(self,x,y,z,duration_ms=1500):
 
-        target_position = [x, y, z]
+        target_position = [x,y,z]
 
         ik_angles = self.chain.inverse_kinematics(
             target_position,
             orientation_mode=None
         )
-
-        self.last_joints = ik_angles
-        self.last_pos = [x, y, z]
 
         s1 = 90 + math.degrees(ik_angles[1])
         s2 = 90 + math.degrees(ik_angles[2])
@@ -54,7 +82,7 @@ class DofbotSimple:
         s4 = 90 + math.degrees(ik_angles[4])
         s5 = 90
 
-        safe = [int(max(0, min(180, a))) for a in [s1, s2, s3, s4, s5]]
+        safe = [int(max(0,min(180,a))) for a in [s1,s2,s3,s4,s5]]
 
         self.Arm.Arm_serial_servo_write6(
             safe[0],
@@ -66,19 +94,16 @@ class DofbotSimple:
             int(duration_ms)
         )
 
+    def set_torque(self,val):
+
+        self.Arm.Arm_serial_set_torque(int(val))
+
     def go_home(self):
 
         self.Arm.Arm_serial_servo_write6(
-            90,
-            90,
-            90,
-            90,
-            90,
+            90,90,90,90,90,
             self.gripper_angle,
             1000
         )
 
         time.sleep(1.5)
-
-        self.last_pos = [0.0, 0.0, 0.0]
-        self.last_joints = [0.0] * 7
